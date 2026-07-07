845baad56b5b1dfce19f75dd5ccc900a7e3334a8
Luis Martinez Sandoval <in3707989@gmail.com>
2026-07-06 19:53:41 -0600

    fix: quitar plugins sitemap y metas de _config.ts (no incluidos en lume v2.5.2)

--- _config.ts
+++ _config.ts
@@ -2,8 +2,6 @@ import lume from "lume/mod.ts";
 import vento from "lume/plugins/vento.ts";
 import markdown from "lume/plugins/markdown.ts";
 import date from "lume/plugins/date.ts";
-import metas from "lume/plugins/metas.ts";
-import sitemap from "lume/plugins/sitemap.ts";
 
 const site = lume({
   src: "./src",
@@ -14,8 +12,6 @@ const site = lume({
 site.use(vento());
 site.use(markdown());
 site.use(date());
-site.use(metas());
-site.use(sitemap());
 
 site.copy("../static", "static");
 
